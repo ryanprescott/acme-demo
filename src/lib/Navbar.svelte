223df15e40a2f924acb47501016ff46d0642d706
Ryan Prescott <rppgoat@gmail.com>
2022-12-29 20:06:18 -0500

    Concise nav

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
     import { page } from '$app/stores';
-    let path: string;
+    let currentPath: string;
 
-    $: path = $page.url.pathname;
+    $: currentPath = $page.url.pathname;
 
     const links = [
-        {text: "Home", link: "/"},
-        {text: "About", link: "/about"},
-        {text: "Products", link: "/products"},
-        {text: "Shop", link: "/shop"},
-        {text: "Contact", link: "/contact"},
+        {text: "Home", path: "/"},
+        {text: "About", path: "/about"},
+        {text: "Products", path: "/products"},
+        {text: "Shop", path: "/shop"},
+        {text: "Contact", path: "/contact"},
     ];
 </script>
 
 <ul class="flex w-fit">
-    {#each links as link}
+    {#each links as {text, path}}
         <li class="px-3 hover:text-red-500 transition-colors">
-            <a href={link.link}>{link.text}</a>
-            {#if path === link.link}
+            <a href={path}>{text}</a>
+            {#if currentPath === path}
                 <div 
                     class="h-0.5 bg-red-500"
                     style:margin-top="-3px"
